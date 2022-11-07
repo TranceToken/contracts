bcec68192b073cf69ac82c08df316fd43d047903
smurf123444 <117327700+TranceToken@users.noreply.github.com>
2022-11-06 19:32:47 -0800

    corrected unused function.,

--- contracts/IRankedMintingToken.sol
+++ contracts/IRankedMintingToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 interface IRankedMintingToken {
-    event RankedClaimed(address indexed user, uint256 term, uint256 rank);
+    event RankClaimed(address indexed user, uint256 term, uint256 rank);
 
     event MintClaimed(address indexed user, uint256 rewardAmount);
 
