0e5150ea371870222e438716ee0a40290626d646
smurf123444 <117327700+TranceToken@users.noreply.github.com>
2022-11-28 17:22:05 -0800

    Self-Audited claim, fixed critical bug. Changed Variable names
    Secured the minting function by changing the minter to account instead of msg.sender, where any one could have claimed for another person, as long as they had the correct proof and index and amount and account, now they are required to be the same person as the account they are claiming as (added a require statement and assigned the struct to the account parameter instead of msg.sender). Will be testing further!

--- contracts/TRANCETOKEN.sol
+++ contracts/TRANCETOKEN.sol
@@ -11,9 +11,6 @@ import "./IRankedMintingToken.sol";
 import "./IBurnableToken.sol";
 import "./IBurnRedeemable.sol";
 
-
-
-
 contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableToken, ERC20("TRANCE Crypto", "TRANCE") {
     using Math for uint256;
     using ABDKMath64x64 for int128;
@@ -37,13 +34,13 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     }
     
 
-    // INTERNAL TYPE TO DESCRIBE A XEN MINT INFO
+    // INTERNAL TYPE TO DESCRIBE A TRANCE MINT INFO
     struct MintInfo {
         address user;
         uint256 amount;
     }
 
-    // INTERNAL TYPE TO DESCRIBE A XEN STAKE
+    // INTERNAL TYPE TO DESCRIBE A TRANCE STAKE
     struct StakeInfo {
         uint256 term;
         uint256 maturityTs;
@@ -63,13 +60,13 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     uint256 public constant MAX_TERM_START = 100 * SECONDS_IN_DAY;
     uint256 public constant MAX_TERM_END = 1_000 * SECONDS_IN_DAY;
 
-    uint256 public constant XEN_MIN_STAKE = 0;
+    uint256 public constant TRANCE_MIN_STAKE = 0;
 
-    uint256 public constant XEN_MIN_BURN = 0;
+    uint256 public constant TRANCE_MIN_BURN = 0;
 
-    uint256 public constant XEN_APY_START = 20;
-    uint256 public constant XEN_APY_DAYS_STEP = 365;
-    uint256 public constant XEN_APY_END = 15;
+    uint256 public constant TRANCE_APY_START = 20;
+    uint256 public constant TRANCE_APY_DAYS_STEP = 365;
+    uint256 public constant TRANCE_APY_END = 15;
 /* 
     string public constant AUTHORS = "@MrJackLevin @lbelyaev faircrypto.org";
  */
@@ -78,12 +75,12 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     uint256 public immutable genesisTs;
     uint256 public activeMinters;
     uint256 public activeStakes;
-    uint256 public totalXenStaked;
-    // user address => XEN mint info
+    uint256 public totalTRANCEStaked;
+    // user address => TRANCE mint info
     mapping(address => MintInfo) public userMints;
-    // user address => XEN stake info
+    // user address => TRANCE stake info
     mapping(address => StakeInfo) public userStakes;
-    // user address => XEN burn amount
+    // user address => TRANCE burn amount
     mapping(address => uint256) public userBurns;
 
     // CONSTRUCTOR
@@ -122,10 +119,11 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
      */
     function _cleanUpUserMint() private {
         delete userMints[_msgSender()];
+        activeMinters--;
     }
 
     /**
-     * @dev calculates XEN Stake Reward
+     * @dev calculates TRANCE Stake Reward
      */
     function _calculateStakeReward(
         uint256 amount,
@@ -144,9 +142,9 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
      * @dev calculates APY (in %)
      */
     function _calculateAPY() private view returns (uint256) {
-        uint256 decrease = (block.timestamp - genesisTs) / (SECONDS_IN_DAY * XEN_APY_DAYS_STEP);
-        if (XEN_APY_START - XEN_APY_END < decrease) return XEN_APY_END;
-        return XEN_APY_START - decrease;
+        uint256 decrease = (block.timestamp - genesisTs) / (SECONDS_IN_DAY * TRANCE_APY_DAYS_STEP);
+        if (TRANCE_APY_START - TRANCE_APY_END < decrease) return TRANCE_APY_END;
+        return TRANCE_APY_START - decrease;
     }
 
     /**
@@ -160,7 +158,7 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
             apy: _calculateAPY()
         });
         activeStakes++;
-        totalXenStaked += amount;
+        totalTRANCEStaked += amount;
     }
 
     // PUBLIC CONVENIENCE GETTERS
@@ -174,7 +172,7 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     }
 
     /**
-     * @dev returns XEN Stake object associated with User account address
+     * @dev returns TRANCE Stake object associated with User account address
      */
     function getUserStake() external view returns (StakeInfo memory) {
         return userStakes[_msgSender()];
@@ -190,14 +188,14 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     // PUBLIC STATE-CHANGING METHODS
 
     /**
-     * @dev accepts User cRank claim provided all checks pass (incl. no current claim exists)
+     * @dev accepts User Claim claim provided all checks pass (incl. no current claim exists)
      */
     function claim(uint256 index, address account, uint256 _amount, bytes32[] calldata merkleProof) external {
-
-        require(!isClaimed(index), "MerkleDistributor: Drop already claimed.");
+        require(account == msg.sender, "Airdrop: Account does not match");
+        require(!isClaimed(index), "Airdrop: Drop already claimed.");
            // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, account, _amount));
-        require(MerkleProof.verify(merkleProof, merkleRoot, node), "MerkleDistributor: Invalid proof.");
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), "Airdrop: Invalid proof.");
         // Mark it claimed.
         _setClaimed(index);
         uint256 reward = _adjustSillyWhale(_amount);
@@ -212,11 +210,11 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
     }
 
     /**
-     * @dev ends minting upon maturity (and within permitted Withdrawal Time Window), gets minted XEN
+     * @dev ends minting upon maturity (and within permitted Withdrawal Time Window), gets minted TRANCE
      */
     function claimMintReward() external {
         MintInfo memory mintInfo = userMints[_msgSender()];
-        require(mintInfo.amount > 0, "CRank: No mint exists");
+        require(mintInfo.amount > 0, "Claim: No mint exists");
 
         // calculate reward and mint tokens
         uint256 rewardAmount = mintInfo.amount;
@@ -228,14 +226,14 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
 
     /**
      * @dev  ends minting upon maturity (and within permitted Withdrawal time Window)
-     *       mints XEN coins and splits them between User and designated other address
+     *       mints TRANCE coins and splits them between User and designated other address
      */
     function claimMintRewardAndShare(address other, uint256 pct) external {
         MintInfo memory mintInfo = userMints[_msgSender()];
-        require(other != address(0), "CRank: Cannot share with zero address");
-        require(pct > 0, "CRank: Cannot share zero percent");
-        require(pct < 101, "CRank: Cannot share 100+ percent");
-        require(mintInfo.amount > 0, "CRank: No mint exists");
+        require(other != address(0), "Claim: Cannot share with zero address");
+        require(pct > 0, "Claim: Cannot share zero percent");
+        require(pct < 101, "Claim: Cannot share 100+ percent");
+        require(mintInfo.amount > 0, "Claim: No mint exists");
 
         // calculate reward
         uint256 rewardAmount = mintInfo.amount;
@@ -252,13 +250,13 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
 
     /**
      * @dev  ends minting upon maturity (and within permitted Withdrawal time Window)
-     *       mints XEN coins and stakes 'pct' of it for 'term'
+     *       mints TRANCE coins and stakes 'pct' of it for 'term'
      */
     function claimMintRewardAndStake(uint256 pct, uint256 term) external {
         MintInfo memory mintInfo = userMints[_msgSender()];
-        // require(pct > 0, "CRank: Cannot share zero percent");
-        require(pct < 101, "CRank: Cannot share >100 percent");
-        require(mintInfo.amount > 0, "CRank: No mint exists");
+        // require(pct > 0, "Claim: Cannot share zero percent");
+        require(pct < 101, "Claim: Cannot share >100 percent");
+        require(mintInfo.amount > 0, "Claim: No mint exists");
 
         // calculate reward
         uint256 rewardAmount = mintInfo.amount;
@@ -272,59 +270,59 @@ contract TRANCECrypto is Context, IRankedMintingToken, IStakingToken, IBurnableT
 
         // nothing to burn since we haven't minted this part yet
         // stake extra tokens part
-        require(stakedReward > XEN_MIN_STAKE, "XEN: Below min stake");
-        require(term * SECONDS_IN_DAY > MIN_TERM, "XEN: Below min stake term");
-        require(term * SECONDS_IN_DAY < MAX_TERM_END + 1, "XEN: Above max stake term");
-        require(userStakes[_msgSender()].amount == 0, "XEN: stake exists");
+        require(stakedReward > TRANCE_MIN_STAKE, "TRANCE: Below min stake");
+        require(term * SECONDS_IN_DAY > MIN_TERM, "TRANCE: Below min stake term");
+        require(term * SECONDS_IN_DAY < MAX_TERM_END + 1, "TRANCE: Above max stake term");
+        require(userStakes[_msgSender()].amount == 0, "TRANCE: stake exists");
 
         _createStake(stakedReward, term);
         emit Staked(_msgSender(), stakedReward, term);
     }
 
     /**
-     * @dev initiates XEN Stake in amount for a term (days)
+     * @dev initiates TRANCE Stake in amount for a term (days)
      */
     function stake(uint256 amount, uint256 term) external {
-        require(balanceOf(_msgSender()) >= amount, "XEN: not enough balance");
-        require(amount > XEN_MIN_STAKE, "XEN: Below min stake");
-        require(term * SECONDS_IN_DAY > MIN_TERM, "XEN: Below min stake term");
-        require(term * SECONDS_IN_DAY < MAX_TERM_END + 1, "XEN: Above max stake term");
-        require(userStakes[_msgSender()].amount == 0, "XEN: stake exists");
+        require(balanceOf(_msgSender()) >= amount, "TRANCE: not enough balance");
+        require(amount > TRANCE_MIN_STAKE, "TRANCE: Below min stake");
+        require(term * SECONDS_IN_DAY > MIN_TERM, "TRANCE: Below min stake term");
+        require(term * SECONDS_IN_DAY < MAX_TERM_END + 1, "TRANCE: Above max stake term");
+        require(userStakes[_msgSender()].amount == 0, "TRANCE: stake exists");
 
-        // burn staked XEN
+        // burn staked TRANCE
         _burn(_msgSender(), amount);
-        // create XEN Stake
+        // create TRANCE Stake
         _createStake(amount, term);
         emit Staked(_msgSender(), amount, term);
     }
 
     /**
-     * @dev ends XEN Stake and gets reward if the Stake is mature
+     * @dev ends TRANCE Stake and gets reward if the Stake is mature
      */
     function withdraw() external {
         StakeInfo memory userStake = userStakes[_msgSender()];
-        require(userStake.amount > 0, "XEN: no stake exists");
+        require(userStake.amount > 0, "TRANCE: no stake exists");
 
-        uint256 xenReward = _calculateStakeReward(
+        uint256 TRANCEReward = _calculateStakeReward(
             userStake.amount,
             userStake.term,
             userStake.maturityTs,
             userStake.apy
         );
         activeStakes--;
-        totalXenStaked -= userStake.amount;
+        totalTRANCEStaked -= userStake.amount;
 
-        // mint staked XEN (+ reward)
-        _mint(_msgSender(), userStake.amount + xenReward);
-        emit Withdrawn(_msgSender(), userStake.amount, xenReward);
+        // mint staked TRANCE (+ reward)
+        _mint(_msgSender(), userStake.amount + TRANCEReward);
+        emit Withdrawn(_msgSender(), userStake.amount, TRANCEReward);
         delete userStakes[_msgSender()];
     }
 
     /**
-     * @dev burns XEN tokens and creates Proof-Of-Burn record to be used by connected DeFi services
+     * @dev burns TRANCE tokens and creates Proof-Of-Burn record to be used by connected DeFi services
      */
     function burn(address user, uint256 amount) public {
-        require(amount > XEN_MIN_BURN, "Burn: Below min limit");
+        require(amount > TRANCE_MIN_BURN, "Burn: Below min limit");
         require(
             IERC165(_msgSender()).supportsInterface(type(IBurnRedeemable).interfaceId),
             "Burn: not a supported contract"
