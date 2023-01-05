aa9b36ab3373cfdce44c85a9972332191deb1d06
Trance Token <117327700+TranceToken@users.noreply.github.com>
2023-01-05 03:11:58 -0800

    removed rankedClaim function prototype.

--- contracts/IRankedMintingToken.sol
+++ contracts/IRankedMintingToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.10;
 
 interface IRankedMintingToken {
-    event RankClaimed(address indexed user, uint256 term, uint256 rank);
 
     event MintClaimed(address indexed user, uint256 rewardAmount);
 
